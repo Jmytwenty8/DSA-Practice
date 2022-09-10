b1335a9b1a4bc798d48caed150fe494c83086eeb
Kumar Jyotirmay <jmy2898@gmail.com>
2022-09-10 12:55:29 +0530

    Time: 236 ms (5.18%), Space: 27 MB (5.92%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,18 +1,58 @@
 class Solution {
 public:
-    
-    
     vector<int> sortedSquares(vector<int>& nums) {
         
-        sort(nums.begin(),nums.end(),[](int a,int b){
-            return abs(a) < abs(b);
-        });
-        
+        int pivot = -1;
         for(int i=0;i<nums.size();i++){
-            nums[i] = nums[i]*nums[i];
+            if(nums[i]>=0){
+                pivot = i;
+                break;
+            }
         }
         
-        return nums;
+        cout<<pivot<<endl;
+        vector<int> ans;
         
+        if(pivot != -1){
+            int i = pivot - 1;
+            int j = pivot;
+
+            while(i>=0 && j<nums.size()){
+                if(abs(nums[i])>abs(nums[j])){
+                    ans.push_back(nums[j]*nums[j]);
+                    j++;
+                }else{
+                    ans.push_back(nums[i]*nums[i]);
+                    i--;
+                }
+            }
+            
+            for(auto i:ans){
+                cout<<i<<" ";
+            }
+            cout<<endl;
+
+            while(i>=0){
+                ans.push_back(nums[i]*nums[i]);
+                i--;
+            }
+
+            while(j<nums.size()){
+                ans.push_back(nums[j]*nums[j]);
+                j++;
+            }
+        }else{
+            if(nums[0]>=0){
+               for(auto i:nums){
+                    ans.push_back(i*i);
+                } 
+            }else{
+                for(int i=nums.size()-1;i>=0;i--){
+                    ans.push_back(nums[i]*nums[i]);
+                }
+            }
+        }
+        
+        return ans;
     }
 };
