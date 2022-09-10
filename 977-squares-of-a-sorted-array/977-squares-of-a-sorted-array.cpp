65b3fedee838ee3c1d9ddb2f3d1f019738f8c1fd
Kumar Jyotirmay <jmy2898@gmail.com>
2022-09-10 13:32:00 +0530

    Time: 211 ms (5.18%), Space: 28.5 MB (5.29%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,58 +1,43 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        
-        int pivot = -1;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]>=0){
-                pivot = i;
-                break;
-            }
+        vector<int>pos;
+        vector<int>neg;
+        for(int i =0 ; i < nums.size() ; i++){
+            if(nums[i] < 0)
+                neg.push_back(nums[i] * nums[i]);
+            else
+                pos.push_back(nums[i] * nums[i]);
+                
         }
-        
-        cout<<pivot<<endl;
-        vector<int> ans;
-        
-        if(pivot != -1){
-            int i = pivot - 1;
-            int j = pivot;
-
-            while(i>=0 && j<nums.size()){
-                if(abs(nums[i])>abs(nums[j])){
-                    ans.push_back(nums[j]*nums[j]);
-                    j++;
-                }else{
-                    ans.push_back(nums[i]*nums[i]);
-                    i--;
-                }
+        reverse(neg.begin(),neg.end());
+        for(int i : pos)
+            cout<<i<<" ";
+        cout<<endl;
+        for(int i : neg)
+            cout<<i<<" ";
+        cout<<endl;
+        vector<int>ans;
+        int i =0 ;
+        int j = 0;
+        while(i < pos.size() && j < neg.size()){
+            if(pos[i] < neg[j]){
+                ans.push_back(pos[i]);
+                i++;
             }
-            
-            for(auto i:ans){
-                cout<<i<<" ";
-            }
-            cout<<endl;
-
-            while(i>=0){
-                ans.push_back(nums[i]*nums[i]);
-                i--;
-            }
-
-            while(j<nums.size()){
-                ans.push_back(nums[j]*nums[j]);
+            else{
+                ans.push_back(neg[j]);
                 j++;
             }
-        }else{
-            if(nums[0]>=0){
-               for(auto i:nums){
-                    ans.push_back(i*i);
-                } 
-            }else{
-                for(int i=nums.size()-1;i>=0;i--){
-                    ans.push_back(nums[i]*nums[i]);
-                }
-            }
         }
-        
+        while(i< pos.size()){
+            ans.push_back(pos[i]);
+            i++;
+        }
+        while(j< neg.size()){
+            ans.push_back(neg[j]);
+            j++;
+        }
         return ans;
     }
 };
