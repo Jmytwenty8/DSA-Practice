25bc9190ec83446abe52308ba782f3ac2e91efbe
Kumar Jyotirmay <jmy2898@gmail.com>
2022-10-12 23:29:50 +0530

    Time: 10 ms (86.48%), Space: 11.2 MB (89.47%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        
+        vector<int> res(1<<n);
+        // res[0] = 0;
+        
+        for(int i=0;i<(1<<n);i++){
+            res[i] = i^(i>>1);
+        }
+        
+        return res;
+        
+    }
+};
