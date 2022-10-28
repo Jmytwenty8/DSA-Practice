30df98e2d4caab822e69d843e45753cf5c8c164a
Kumar Jyotirmay <jmy2898@gmail.com>
2022-10-29 00:49:13 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (15.68%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
-    int climbStairs(int n) {
+//     int climbStairs(int n) {
         
-        vector<int> dp(n+1,-1);
-        solve(n,dp);
+//         vector<int> dp(n+1,-1);
+//         solve(n,dp);
         
-        return dp[n];
-    }
+//         return dp[n];
+//     }
     
-    int solve(int n, vector<int>& dp){
-        if(n == 0){
-            return 1;
+//     int solve(int n, vector<int>& dp){
+//         if(n == 0){
+//             return 1;
+//         }
+        
+//         if(dp[n] != -1) return dp[n];
+//         int oneStep = 0;
+//         if(n > 0) oneStep = solve(n-1,dp);
+//         int twoStep = 0;
+//         if(n > 1) twoStep = solve(n-2,dp);
+        
+//         return dp[n] = oneStep + twoStep;
+//     }
+    
+    int climbStairs(int n){
+        vector<int> dp(n+1);
+        
+        dp[0] = dp[1] = 1;
+        
+        for(int i=2;i<n+1;i++){
+            dp[i] = dp[i-1]+dp[i-2];
         }
         
-        if(dp[n] != -1) return dp[n];
-        int oneStep = 0;
-        if(n > 0) oneStep = solve(n-1,dp);
-        int twoStep = 0;
-        if(n > 1) twoStep = solve(n-2,dp);
-        
-        return dp[n] = oneStep + twoStep;
+        return dp[n];
     }
 };
