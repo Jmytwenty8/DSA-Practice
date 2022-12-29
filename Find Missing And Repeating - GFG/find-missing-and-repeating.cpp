af1db6c8024eeb55d905b7ce7df5f6a8752e3fcb
Kumar Jyotirmay <jmy2898@gmail.com>
2022-12-29 15:16:11 +0530

    Added solution - LeetHub

---
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int *findTwoElement(int *arr, int n) {
+        // code here
+        
+            int xor1;
+    
+        /* Will have only single set bit of xor1 */
+        int set_bit_no;
+    
+        int i;
+        int x = 0; // missing
+        int y = 0; // repeated
+        // int n = arr.size();
+    
+        xor1 = arr[0];
+    
+        /* Get the xor of all array elements */
+        for (i = 1; i < n; i++)
+            xor1 = xor1 ^ arr[i];
+    
+        /* XOR the previous result with numbers from 1 to n */
+        for (i = 1; i <= n; i++)
+            xor1 = xor1 ^ i;
+    
+        /* Get the rightmost set bit in set_bit_no */
+        set_bit_no = xor1 & ~(xor1 - 1);
+    
+        /* Now divide elements into two sets by comparing a rightmost set bit
+           of xor1 with the bit at the same position in each element.
+           Also, get XORs of two sets. The two XORs are the output elements.
+           The following two for loops serve the purpose */
+    
+        for (i = 0; i < n; i++) {
+            if (arr[i] & set_bit_no)
+                /* arr[i] belongs to first set */
+                x = x ^ arr[i];
+    
+            else
+                /* arr[i] belongs to second set */
+                y = y ^ arr[i];
+        }
+    
+        for (i = 1; i <= n; i++) {
+            if (i & set_bit_no)
+                /* i belongs to first set */
+                x = x ^ i;
+    
+            else
+                /* i belongs to second set */
+                y = y ^ i;
+        }
+    
+        // NB! numbers can be swapped, maybe do a check ?
+        int x_count = 0;
+        for (int i=0; i<n; i++) {
+            if (arr[i]==x)
+                x_count++;
+        }
+        
+        int* res = new int[2]; 
+        if (x_count==0){
+            res[0] = y;
+            res[1] = x;
+        }else{
+            res[0] = x;
+            res[1] = y;
+        }
+        
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findTwoElement(a, n);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
